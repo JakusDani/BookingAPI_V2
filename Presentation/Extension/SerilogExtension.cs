64deec3ea94e679173ca68835c89a5d5385fa383
Jakus Daniel <skelox0@gmail.com>
2024-01-23 22:51:13 +0100

    Fix - remove serilog file config because put log file in the root dir and doesnt matter what i write in the config file

--- Presentation/Extension/SerilogExtension.cs
+++ Presentation/Extension/SerilogExtension.cs
@@ -2,21 +2,26 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
+using Serilog.Formatting.Compact;
 
 namespace Presentation.Extension;
 public static class SerilogExtension
 {
     public static ILoggingBuilder SerilogConfig(this ILoggingBuilder loggingBuilder)
     {
-        string currentDir = AppDomain.CurrentDomain.BaseDirectory
-;
+        string currentDir = AppDomain.CurrentDomain.BaseDirectory;
         IConfigurationRoot config = new ConfigurationBuilder()
             .SetBasePath(currentDir)
             .AddJsonFile("appsettings.json")
             .Build();
 
+        string loggerPath = currentDir + "Logs/log.json";
         Logger logger = new LoggerConfiguration()
             .ReadFrom.Configuration(config)
+            .WriteTo.File(new CompactJsonFormatter(),
+                loggerPath,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 60)
             .CreateLogger();
 
         return loggingBuilder.AddSerilog(logger);
